24f5b125c81bdc9559624ce5b3c33588db9e8b00
21Miloo <camilo052110@gmail.com>
2026-01-13 23:19:45 -0500

    change-color-DOM

---
+++ app.js
@@ -0,0 +1,34 @@
+const button = document.querySelector("#boton-color")
+const color = document.getElementById("color")
+
+button.addEventListener('click', applyRandomColor)
+
+
+
+// Functions
+
+function generateColorHex(){
+    let digits = '0123456789ABCDEF'; // <-- Code Hex
+    let colorHex = '#';
+
+    for (let i=0; i < 6; i++){
+
+        let indiceAleatorio = Math.floor(Math.random() * 16);
+        colorHex += digits[indiceAleatorio];
+    }
+
+    return colorHex;
+
+}
+
+function applyRandomColor () {
+
+    let randomColor = generateColorHex();
+
+    // Actualizamos el Texto
+    color.textContent = randomColor;
+
+    // Actualizamos el color de fondo
+    document.body.style.backgroundColor = randomColor;
+
+}
